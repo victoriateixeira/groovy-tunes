595aeb663102ac3ed19431ec072bc92a947f1743
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-10-04 16:24:31 +1000

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,49 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameArtist: '',
+      // isLoading: false,
+    };
+  }
+
+  activateSearchButton = () => {
+    const { nameArtist } = this.state;
+    const minLength = 2;
+    return nameArtist.length >= minLength;
+  };
+
+  searchArtistName = (event) => {
+    this.setState({
+      nameArtist: event.target.value,
+    });
+  };
+
   render() {
     return (
-      <div data-testid="page-search">
-        <Header />
-      </div>
+      <>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <div>
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.searchArtistName }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ !this.activateSearchButton() }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
+      </>
     );
   }
 }
