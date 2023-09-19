20f3f1c3d6a1ced9a6995ab740e73daa637461f3
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-09-19 18:41:37 -0300

    feat: cria a página de perfil do usuário

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      isLoading: false,
+    };
+  }
+
+  async componentDidMount() {
+    await this.retrieveUser();
+  }
+
+  retrieveUser = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const user = await getUser();
+    console.log(user);
+    this.setState({
+      user,
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { user, isLoading } = this.state;
     return (
+
       <div data-testid="page-profile">
         <Header />
+        {isLoading ? <Loading />
+          : (
+            <div>
+              <Link to="/profile/edit">Editar perfil</Link>
+              <img src={ user.image } alt="" />
+              <p>
+                Name:
+                {user.name}
+              </p>
+              <p>
+                E-mail:
+
+                {user.email}
+              </p>
+              <p>
+                Description:
+                {user.description}
+              </p>
+            </div>
+          )}
+
       </div>
     );
   }
